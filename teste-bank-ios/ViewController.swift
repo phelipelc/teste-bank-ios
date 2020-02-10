447f78aede80e6c18f661241a2fb03102e47a676
user150042 <user150042@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-02-10 16:30:18 -0500

    Initial Commit

---
+++ teste-bank-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  teste-bank-ios
+//
+//  Created by user150042 on 2/10/20.
+//  Copyright © 2020 Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ teste-bank-iosTests/teste_bank_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  teste_bank_iosTests.swift
+//  teste-bank-iosTests
+//
+//  Created by user150042 on 2/10/20.
+//  Copyright © 2020 Lopes. All rights reserved.
+//
+
+import XCTest
+@testable import teste_bank_ios
+
+class teste_bank_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
