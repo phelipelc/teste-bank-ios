411aaec0e04656ac211715efe1276cdf0fdb7f2d
user150042 <user150042@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-02-10 16:46:55 -0500

    first screen without storyboard and base folders.

--- teste-bank-ios/Scenes/Login/LoginViewController.swift
+++ teste-bank-ios/Scenes/Login/LoginViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
         // Do any additional setup after loading the view, typically from a nib.
     }
 
