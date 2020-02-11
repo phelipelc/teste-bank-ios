0f880a8127e4dfb35d0299188a0de176b1f09870
user150042 <user150042@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-02-11 12:06:29 -0500

    rule validation of Fields

--- teste-bank-ios/Scenes/Login/LoginPresenter.swift
+++ teste-bank-ios/Scenes/Login/LoginPresenter.swift
@@ -7,3 +7,10 @@
 //
 
 import Foundation
+protocol LoginPresenterProtocol {
+    
+}
+
+class LoginPresenter {
+    
+}
--- teste-bank-ios/Scenes/Login/LoginViewController.swift
+++ teste-bank-ios/Scenes/Login/LoginViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import DTTextField
+protocol LoginViewProtocol {
+    func showTextFieldError(textFields: DTTextField...)
+}
 
 class LoginViewController: UIViewController {
+    @IBOutlet weak var btnLogin: UIButton!
+    @IBOutlet weak var txtPassword: DTTextField!
+    @IBOutlet weak var txtUser: DTTextField!
+    
     var router: LoginRouterProtocol?
     var interactor: LoginInteractorProtocol?
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayout()
+        addTarget()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func addTarget(){
+        btnLogin.addTarget(self, action: #selector(doLogin), for: .touchUpInside)
+    }
+    @objc func doLogin() {
+        if(txtUser.text!.isEmpty) {
+            showTextFieldError(textFields: txtUser)
+            return
+        }
+        
+        if(txtPassword.text!.isEmpty) {
+            showTextFieldError(textFields: txtPassword)
+            return
+        }
+        
+         interactor?.doLogin(user: txtUser.text!, password: txtPassword.text!)
+        
+    }
+    
+    func showTextFieldError(textFields: DTTextField...) {
+        for field in textFields {
+            field.showError(message: "\(field.placeholder) invalido")
+        }
     }
     
     func setupLayout() {
